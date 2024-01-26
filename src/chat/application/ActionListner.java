006692107185a3652930f20c7d7fca5c110fb5df
amitkurmi014 <155133519+amitkurmi014@users.noreply.github.com>
2024-01-26 16:03:25 +0545

    first commit

---
+++ src/chat/application/ActionListner.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package chat.application;
+
+/**
+ *
+ * @author A S P I R E 7
+ */
+interface ActionListner {
+    
+}
